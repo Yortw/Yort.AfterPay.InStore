daaba3dacada2c6982ed88d17a67292bf3e20d7a
Troy Willmot <yort@yortondotnet.com>
2017-09-29 21:46:30 +1300

    Add Net 4.5 project to reduce dependencies for later frameworks.

---
+++ src/Yort.AfterPay.InStore.Net45/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Yort.AfterPay.InStore.Net45")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1323da2e-eeda-475d-b299-fd8233b55fd6")]
